c729ade72bfe3e1943f10881b17bc8c9c09b6e78
Melvin Ugochukwu <ugochukwumelvin@gmail.com>
2023-10-01 14:36:54 -1100

    dependency/next-reveal

--- components/ProductsGrid.tsx
+++ components/ProductsGrid.tsx
@@ -1,12 +1,18 @@
+"use client";
 import React from "react";
 import ProductBox from "./ProductBox";
 import { Product } from "@/types";
+import { RevealWrapper } from "next-reveal";
 
 const ProductsGrid = ({ products }: { products: Product[] }) => {
   return (
     <div className=" py-20 center grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-7">
       {products.map((product, index) => {
-        return <ProductBox key={index} product={product} />;
+        return (
+          <RevealWrapper key={index} delay={index * 50}>
+            <ProductBox product={product} />
+          </RevealWrapper>
+        );
       })}
     </div>
   );
